da1096f797915591418dc95ab4aed6b67e94c9a6
hefengfan <56315837@qq.com>
2025-01-05 11:26:24 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,16 @@
-
-addEventListener("activate", event => {
-	event.waitUntil(clients.claim());
-});
-
-// Credit: https://stackoverflow.com/a/68675301/9953396
 addEventListener("fetch", function(event) {
-	if(event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") {
+	if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") {
 		return;
 	}
 
 	event.respondWith(
 		fetch(event.request)
 			.then(function(response) {
+				// Check if the response is valid
+				if (!response || response.status < 200 || response.status >= 600) {
+					return;
+				}
+
 				const newHeaders = new Headers(response.headers);
 				newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
 				newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
