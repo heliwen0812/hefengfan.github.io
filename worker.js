c8f8a99655f60bfcbe4e44418e8f789fa4dacfc9
hefengfan <56315837@qq.com>
2025-01-05 11:07:01 +0800

    Delete worker.js

--- worker.js
+++
@@ -1,8 +0,0 @@
-
-addEventListener("message", event => {
-	const buffer = event.data;
-	const array = new Uint8Array(buffer);
-	array[0] = 100;
-	console.log("Worker writing 100 to the buffer");
-	postMessage("ok");
-});
